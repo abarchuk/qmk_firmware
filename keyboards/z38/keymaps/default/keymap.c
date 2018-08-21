12d4c97108d238bee27aad57bf6b62a1b37d4681
Zver Zvereff <andreibarchuk@gmail.com>
2018-08-21 10:17:24 +0300

    Updated z38 keymap

--- keyboards/z38/keymaps/default/keymap.c
+++ keyboards/z38/keymaps/default/keymap.c
@@ -22,23 +22,23 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DEFAULT] = {
-    { KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_TRNS, KC_Y,      KC_U,    KC_I,    KC_O,    KC_P    },
-    { KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_TRNS, KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN },
-    { KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_LSFT, KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
-    { KC_TRNS, KC_TRNS, KC_TRNS, KC_LCTL, MO(_LOWER), KC_SPC,  MO(_RAISE),KC_LALT, MO(MED), KC_TRNS, KC_TRNS }
+    { KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TRNS, KC_Y,      KC_U,      KC_I,    KC_O,    KC_P    },
+    { KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TRNS, KC_H,      KC_J,      KC_K,    KC_L,    KC_SCLN },
+    { KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,  KC_N,      KC_M,      KC_COMM, KC_DOT,  KC_SLSH },
+    { KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT, KC_LSFT, KC_SPC,  MO(_RAISE),MO(_LOWER),MO(MED), KC_TRNS, KC_TRNS }
 },
 
 [_RAISE] = { 
     { KC_GRV,  KC_TILD, KC_LPRN, KC_RPRN, KC_TAB,  KC_TRNS, KC_BSPC, KC_PGUP, KC_HOME, KC_PIPE, KC_BSLS },
     { KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, KC_ESC,  KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DEL  },
-    { KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_ENT,  KC_TRNS, KC_INS,  KC_PGDN, KC_END,  KC_DQT,  KC_QUOT },
+    { KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_INS,  KC_TRNS, KC_ENT,  KC_PGDN, KC_END,  KC_DQT,  KC_QUOT },
     { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
 },
 
 [_LOWER] = {
     { KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0    },
-    { KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_F11,  KC_F12  },
-    { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10  },
+    { KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN },
+    { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS },
     { KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
 },
 
