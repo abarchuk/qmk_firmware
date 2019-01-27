796702a3188c9f0c56765b8afff240ddc8e56129
Zver Zvereff <andreibarchuk@gmail.com>
2019-01-28 00:00:39 +0300

    Updated atreus62 layout

--- keyboards/atreus62/keymaps/zaj4k/keymap.c
+++ keyboards/atreus62/keymaps/zaj4k/keymap.c
@@ -21,23 +21,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{ KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL  },
 		{ KC_HOME, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_PGUP },
 		{ KC_END,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PGDN },
-		{ _______, KC_LCTL, _______, KC_LCTL, KC_LALT, KC_LSFT, KC_SPC,  MO(SYMB),MO(NUM), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT }
+		{ KC_LGUI, KC_MINS, KC_EQL,  KC_LCTL, KC_LALT, KC_LSFT, KC_SPC,  MO(SYMB),MO(NUM), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT }
 },
 
 [SYMB] = { 
-		{ _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  },
-		{ _______, KC_TILD, KC_GRV,  KC_LPRN, KC_RPRN, KC_TAB,  _______, KC_BSPC, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_F12  },
+		{ _______, _______, _______, _CP,     _PST,    _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, _______ },
+		{ _______, KC_TILD, KC_GRV,  KC_LPRN, KC_RPRN, KC_TAB,  _______, KC_BSPC, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______ },
 		{ _______, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, KC_ESC,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PIPE, _______ },
 		{ _______, _______, _______, KC_DQT,  KC_QUOT, KC_DEL,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSLS, _______ },
 		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ }
 },
 
 [NUM] = { 
-		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-		{ _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_VOLU },
-		{ _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_VOLD },
-		{ _______, _______, _______, _CP,     _PST,    _______, _______, _______, KC_PLUS, KC_EQL,  KC_SLSH, KC_ASTR, KC_MPLY },
-		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT }
+		{ _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  },
+		{ _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12  },
+		{ _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, KC_MINS, KC_PLUS, KC_EQL,  KC_SLSH, KC_ASTR, _______ },
+		{ _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______, _______, _______, _______, _______, _______ },
+		{ _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ }
 }
 
 };
